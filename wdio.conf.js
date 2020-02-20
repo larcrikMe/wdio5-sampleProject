a1baf91987da197b9c6741a4eca1f4ab06eebb5e
Larisa Gota <lgota@smallfootprint.com>
2020-02-20 14:13:37 +0200

    3rd commit

--- wdio.conf.js
+++ wdio.conf.js
@@ -136,7 +136,11 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec'],
+    reporters: [
+        'spec',
+        ['junit', {
+            outputDir: './output'
+        }]],
  
     //
     // Options to be passed to Mocha.
